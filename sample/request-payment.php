10be7b4a0a7a1f384c5d10f7a97bfdc5d44e12e2
soltanihossein <h.soltani2007@gmail.com>
2018-07-19 10:18:35 +0430

    verify and rollback payment sample

---
+++ sample/request-payment.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mabna Payment</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<?php
+
+use Dpsoft\Mabna\MabnaPayment;
+
+require "../vendor/autoload.php";
+
+$request = new MabnaPayment(61000063);
+
+$invoiceId = $request->payRequest($_POST['callbackurl'], $_POST['amount'], $_POST['payload']);
+
+echo $request->getRedirectScript();
+
+?>
